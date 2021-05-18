9e1948c94d186cb8cb625876b96e0cb4fc19f592
Vera-Alejandro <Alejandro.Vera-Gonzalez@trojans.dsu.edu>
2021-05-18 15:31:39 -0500

    Saving Data to DB

--- Tasker/Startup.cs
+++ Tasker/Startup.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mime;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -25,6 +26,8 @@ namespace Tasker
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+
+            services.AddDbContext<ApplicationDbContext>(options => options.UseInMemoryDatabase("Tasker"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
